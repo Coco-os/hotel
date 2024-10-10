9b25dffcde5fe4076c5d5e5ec84fb46e5531dc44
Coco-os <as.gonzalezhz@gmail.com>
2024-10-10 18:46:37 +0100

    cookies

--- Hotel/assets/js/main.js
+++ Hotel/assets/js/main.js
@@ -3,6 +3,26 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 (function($) {
 
